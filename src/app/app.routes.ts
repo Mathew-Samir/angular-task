1b942507427518a4300c8251ca59388524494a95
Mathew-Samir <mthwsamir@gmail.com>
2026-01-15 20:50:49 +0200

    add footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { IssuesList } from './pages/issues-list/issues-list';
+import { IssueDetails } from './pages/issue-details/issue-details';
+import { NotFound } from './pages/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: IssuesList, title: 'Home' },
+  { path: 'product-details/:id', component: IssueDetails, title: 'Product Details' },
+  { path: '**', component: NotFound, title: 'Not Found' },
+];
