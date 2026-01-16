ff099d4a4d8522aead5f446b7965ada537d188fd
Mathew-Samir <mthwsamir@gmail.com>
2026-01-17 00:43:02 +0200

    feat: implement a dedicated 404 Not Found page with its component, template, styles, and an accompanying image.

--- src/app/pages/not-found/not-found.ts
+++ src/app/pages/not-found/not-found.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-not-found',
-  imports: [],
+  imports: [RouterLink, NgOptimizedImage],
   templateUrl: './not-found.html',
   styleUrl: './not-found.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NotFound {
-
 }
