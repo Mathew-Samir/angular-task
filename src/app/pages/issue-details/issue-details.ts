905481bfacb7e61c3b3306905af5a7083baf3465
Mathew-Samir <mthwsamir@gmail.com>
2026-01-17 00:42:45 +0200

    feat: add 404 Not Found page, product creation, and product details pages.

--- src/app/pages/issue-details/issue-details.ts
+++ src/app/pages/issue-details/issue-details.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { Products } from '../../core/services/products/products';
+import { Product } from '../../core/interfaces/products.interface';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-issue-details',
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './issue-details.html',
   styleUrl: './issue-details.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class IssueDetails {
+export class IssueDetails implements OnInit {
+  private readonly route = inject(ActivatedRoute);
+  private readonly productsService = inject(Products);
+  private readonly destroyRef = inject(DestroyRef);
 
+  readonly product = signal<Product | null>(null);
+  readonly isLoading = signal(true);
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.getProduct(id);
+    }
+  }
+
+  getProduct(id: string): void {
+    this.productsService.getProductById(id)
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe({
+        next: (res) => {
+          this.product.set(res);
+          this.isLoading.set(false);
+        },
+        error: (err) => {
+          console.error('Error fetching product:', err);
+          this.isLoading.set(false);
+        }
+      });
+  }
 }
